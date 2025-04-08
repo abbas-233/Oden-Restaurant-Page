5432c51ac533dfd75755a5b6dc423596e192439c
abbas-233 <sor.drop@gmail.com>
2025-04-08 15:51:54 +0300

    push new updates -clonning matching

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,9 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    clean: true, // Cleans the output directory before each build
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
-  mode: 'development',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    compress: true,
-    port: 9000,
-    open: true, // Automatically opens the browser
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', // Template for the HTML file
-      filename: 'index.html',
-    }),
-  ],
 };
